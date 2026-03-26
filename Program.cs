0eae44ef0cf0cf2b3fd27c9be1abf164c3edd232
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-03-26 12:44:40 +0100

    Made a simple funtion to sum an array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿namespace Kalk;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -6,36 +8,56 @@ class Program
     
     static void Main(string[] args)
     {
-        var (val1, val2, opChoice) = userInput();
-        calcResults(val1, val2, opChoice);
+
+        addArray();
+
     }
     
-    static (int, int, string) userInput()
+    public static void addArray()
     {
-        Console.Write("Enter the first number: ");
-        int n1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter the list of numbers you wish to add together: ");
+        double []inputArray = Console.ReadLine()
+                            .Split(' ')
+                            .Select(double.Parse)
+                            .ToArray();
         
-        Console.Write("Enter the second number: ");
-        int n2 = int.Parse(Console.ReadLine());
-        
-        Console.Write("Enter the operator, either +, -, *, /  : ");
-        string op = Console.ReadLine().Trim().ToLower();
-        
-        return (n1, n2, op);
-    }
-    static void calcResults(int n1, int n2, string op)
-    {
-        string result = op switch
+        double total = 0;
+        for (int i = 0; i < inputArray.Length; i++)
         {
-            "+" =>$"The sum of {n1} pluss {n2} is: "                + (n1 + n2),
-            "*" =>$"The product of {n1} times {n2} is: "            + (n1 * n2),
-            "-" =>$"The difference between {n1} minus {n2} is: "    + (n1 - n2),
-            "/" =>$"The quotient between {n1} divided by {n2} is: " + (n1 / n2),
-            _   =>"Feil"
-        };
-        Console.WriteLine(result);
-        
+            total += inputArray[i];
+        } 
+        Console.WriteLine($" The sum of the numbers is: {total}");
     }
+  
+  
+  
+  
+    // static (int, int, string) userInput()
+    // {
+    //     Console.Write("Enter the first number: ");
+    //     int n1 = int.Parse(Console.ReadLine());
+        
+    //     Console.Write("Enter the second number: ");
+    //     int n2 = int.Parse(Console.ReadLine());
+        
+    //     Console.Write("Enter the operator, either +, -, *, /  : ");
+    //     string op = Console.ReadLine().Trim().ToLower();
+        
+    //     return (n1, n2, op);
+    // }
+    // static void calcResults(int n1, int n2, string op)
+    // {
+    //     string result = op switch
+    //     {
+    //         "+" =>$"The sum of {n1} pluss {n2} is: "                + (n1 + n2),
+    //         "*" =>$"The product of {n1} times {n2} is: "            + (n1 * n2),
+    //         "-" =>$"The difference between {n1} minus {n2} is: "    + (n1 - n2),
+    //         "/" =>$"The quotient between {n1} divided by {n2} is: " + (n1 / n2),
+    //         _   =>"Feil"
+    //     };
+    //     Console.WriteLine(result);
+        
+    // }
     
     
     
